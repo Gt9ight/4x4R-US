0776747dd461bc4d226acee3e40f05080afe68b9
Mariano Sanchez Hernandez <marianosanchez0307@gmail.com>
2024-11-03 06:59:58 -0500

    added some images to our brands and services

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,13 @@
 import React, { useState, useRef } from 'react';
 import './home.css'; // Ensure this file includes the new styles
+import Bilstein from './bilstein.png'
+import Rockcrawler from './rockcrawler.png'
+import Fox from './fox.png'
+import RoughCountry from './roughcountry.png'
+import suspension from './suspension.webp'
+import gears from './gears.jpg'
+import wheel from './wheel.webp'
+import bumper from './bumper.png'
 
 
 function Home() {
@@ -12,7 +20,21 @@ function Home() {
         setIsOpen(!isOpen);
     };
 
-    const mockBrands = ['Brand A', 'Brand B', 'Brand C', 'Brand D', 'Brand E', 'Brand F'];
+    const mockBrands = [
+        { name: 'Bilstein', imageSrc: Bilstein },
+        { name: 'Rock Crawler', imageSrc: Rockcrawler },
+        { name: 'Fox', imageSrc: Fox },
+        { name: 'RoughCountry', imageSrc: RoughCountry },
+
+    ];
+
+    const service = [
+        { name: 'SUSPENSION', imageSrc: suspension },
+        { name: 'TIRES & WHEEL', imageSrc: wheel },
+        { name: 'GEARS & DRIVETRAIN', imageSrc: gears },
+        { name: 'BUMPERS, FENDERS, ETC.', imageSrc: bumper },
+
+    ];
 
     return (
         <div>
@@ -41,10 +63,11 @@ function Home() {
             <h2 className='brand-header'>Our Brands</h2>
             <div className="brands-container">
                 
-                <div className="brands-row">
+            <div className="brands-row">
                     {mockBrands.map((brand, index) => (
                         <div key={index} className="brand-item">
-                            {brand}
+                            <img src={brand.imageSrc} alt={brand.name} className="brand-image" />
+                            
                         </div>
                     ))}
                 </div>
@@ -55,6 +78,20 @@ function Home() {
                 <p>
                     At our shop, our goal is to provide the best off-road modifications and unforgettable experiences for off-road enthusiasts. We are passionate about quality, performance, and helping you push the limits of your vehicle in any terrain. Whether you’re looking for expert advice, high-quality upgrades, or a community of like-minded adventurers, we’re here to make it happen!
                 </p>
+            </div>
+
+
+            <h2 className='service-header'>Our Services</h2>
+            <div className="service-container">
+                
+            <div className="service-row">
+                    {service.map((services, index) => (
+                        <div key={index} className="service-item">
+                            <p>{services.name}</p>
+                            <img src={services.imageSrc} alt={services.name} className="service-image" />
+                        </div>
+                    ))}
+                </div>
             </div>
 
 
